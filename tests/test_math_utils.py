9c221b057d6e81de0c3f5f3962c06bc8fdfd62ea
Sudish30 <sudish30chefcurry@gmail.com>
2026-07-08 22:10:47 -0700

    Update tests/test_math_utils.py (agent/issue-1)

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,6 +1,20 @@
-from src.math_utils import add
+from src.math_utils import multiply
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+
+def test_multiply_two_positive_integers():
+    assert multiply(3, 4) == 12
+
+
+def test_multiply_by_zero():
+    assert multiply(5, 0) == 0
+    assert multiply(0, 5) == 0
+
+
+def test_multiply_negative_numbers():
+    assert multiply(-2, 3) == -6
+    assert multiply(-2, -3) == 6
+
+
+def test_multiply_one_as_identity():
+    assert multiply(7, 1) == 7
+    assert multiply(1, 7) == 7
