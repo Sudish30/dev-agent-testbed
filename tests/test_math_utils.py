ca9e70106c41175914680f20513c983da37ae048
Sudish30 <sudish30chefcurry@gmail.com>
2026-07-08 22:21:42 -0700

    Update tests/test_math_utils.py (agent/issue-1)

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,23 +1,45 @@
-from src.math_utils import multiply
+from src.math_utils import add, subtract, multiply
 
 
-def test_multiply_two_positive_integers():
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+
+def test_add_negative_numbers():
+    assert add(-1, -1) == -2
+
+
+def test_add_zero():
+    assert add(0, 5) == 5
+
+
+def test_subtract_positive_numbers():
+    assert subtract(10, 4) == 6
+
+
+def test_subtract_negative_numbers():
+    assert subtract(-3, -2) == -1
+
+
+def test_subtract_zero():
+    assert subtract(7, 0) == 7
+
+
+def test_multiply_positive_numbers():
     assert multiply(3, 4) == 12
 
 
 def test_multiply_by_zero():
     assert multiply(5, 0) == 0
-    assert multiply(0, 5) == 0
 
 
-def test_multiply_two_negative_integers():
-    assert multiply(-3, -4) == 12
+def test_multiply_negative_numbers():
+    assert multiply(-2, -3) == 6
 
 
-def test_multiply_positive_and_negative():
+def test_multiply_mixed_sign():
     assert multiply(-3, 4) == -12
 
 
 def test_multiply_by_one():
     assert multiply(7, 1) == 7
-    assert multiply(1, 7) == 7
